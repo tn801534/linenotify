a27129496f4545fb3c8f9a7bc3fefb1ac19df95e
tn801534 <tn801534@msn.com>
2020-02-13 17:42:23 +0800

    Update main.go

--- main.go
+++ main.go
@@ -77,6 +77,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("result:", res)
 	token = res.AccessToken
 	fmt.Println("write token")
+	f, err := os.OpenFile("tokens", os.O_APPEND|os.O_WRONLY, 0644) 
+	f.Close()
 	fmt.Println("write token done")
 	w.Write(byt)
 }
